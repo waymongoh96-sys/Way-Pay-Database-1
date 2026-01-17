8a80a64014e2133baa0a46bea0e4a7cccb351cc5
waymongoh96-sys <waymon.goh96@gmail.com>
2026-01-17 15:44:21 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // This pulls in the big code you just pasted
-import './index.css' // (Optional, if you have styles)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
